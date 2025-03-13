1e05b66715422d793ffc820131cf72200d9764a2
Vincent Mai <maiv8964@my.yorku.ca>
2025-03-13 17:46:43 -0400

    please

--- EAuction/src/main/java/com/eauction/Item.java
+++ EAuction/src/main/java/com/eauction/Item.java
@@ -1,70 +1,69 @@
 package com.eauction;
-
+// bro please
 public class Item {
 
-    private int id;
-    private String name;
-    private String condition;
-    private String auctionType;
-    private float currentPrice;
-    private String description;
-    private String remainingTime;
+	private int id;
+	private String name;
+	private String condition;
+	private float price;
+	private String description;
+	private String auctionType;
+	private Integer sellerId;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCondition() {
-        return condition;
-    }
-
-    public void setCondition(String condition) {
-        this.condition = condition;
-    }
-
-    public float getCurrentPrice() {
-        return currentPrice;
-    }
-
-    public void setCurrentPrice(float currentPrice) {
-        this.currentPrice = currentPrice;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    
-    public String getAuctionType() {
-        return auctionType;
-    }
-
-    public void setAuctionType(String auctionType) {
-        this.auctionType = auctionType;
-    }
-    
-    public void setRemainingTime(String remainingTime) {
-		this.remainingTime = remainingTime;
+	public int getId() {
+		return id;
 	}
 
-	public String getRemainingTime() {
-		return this.remainingTime;
+	public void setId(int id) {
+		this.id = id;
 	}
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAuctionType() {
+		return auctionType;
+	}
+
+	public void setAuctionType(String auctionType) {
+		this.auctionType = auctionType;
+	}
+
+	public Integer getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(Integer sellerId) {
+		this.sellerId = sellerId;
+	}
 
 }
